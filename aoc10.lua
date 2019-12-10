5d0658ac85c52d7e7d66cd4dfa1de97d858cf723
bjorn roberg <bjorn.roberg@gmail.com>
2019-12-11 00:36:08 +0100

    10

--- aoc10.lua
+++ aoc10.lua
@@ -6,6 +6,8 @@ local prototype = require('prototype')
 local fmt = require('fmt')
 local strings = require('strings')
 local tables = require('tables')
+local func = require('func')
+local numerics = require('numerics')
 
 local tap = require('tap').new {name = 'AoC 10'}
 
@@ -70,44 +72,6 @@ local algorithmicTemplate = [[
 ...e..d..c
 ]]
 
-local numerics = require('numerics')
--- print(numerics.gcd(4,4))
--- print(numerics.lcm(4,4))
-
-local function fraction (v, err)
-  err = err or 0.0001
-  local f = v - math.floor(v)
-  local lo = {0, 1}
-  local hi = {1, 1}
-  while true do
-    -- mid = (lo + hi)/2
-    -- if lo = a/b and hi = c/d, then mid = (ad+bc)/(2ad)
-    local mid = {lo[1]*hi[2] + hi[1]*lo[2], 2*lo[2]*hi[2]}
-    -- gcd to reduce fraction
-    local k = numerics.gcd(mid[1], mid[2])
-    mid = {mid[1]/k, mid[2]/k}
-
-    local d = 1.*mid[1]/mid[2]
-    fmt.dump {mid=mid,d=d,lo=lo,hi=hi,f=f,k=k}
-    -- are we close enough?
-    if math.abs(f - d) < err then
-      break
-    -- if we are above our goal, get high to middle
-    elseif d > f then
-      hi = mid
-    -- if we are under our goal, get lower to middle
-    else
-      lo = mid
-      -- Add integer part
-      mid = {mid[1] + math.floor(v)*mid[2], mid[2]}
-      -- Debug comparing to Fraction library solution.
-      --print v, mid, Fraction('%s' % v)
-    end
-  end
-  return mid
-end
--- fmt.dump(fraction(0.8))
-
 local function traceLine (x1, y1, x2, y2, callback)
   local dx, dy = x2 - x1, y2 - y1
   print(dx, dy)
@@ -142,13 +106,14 @@ end
 tap:addTest(
   'One',
   function (test)
-    local mapData = [[
-.#..#
-.....
-#####
-....#
-...##]]
+--     local mapData = [[
+-- .#..#
+-- .....
+-- #####
+-- ....#
+-- ...##]]
     local map = parse(mapData)
+    -- local map = parse(algorithmicTemplate)
 
     local withinBounds = func.memoize2(function (x, y)
         return (0 < x)
@@ -169,23 +134,34 @@ tap:addTest(
         p = frame[i]
         x, y = p[1], p[2]
         dx, dy = cx-x,cy-y
-        q = numerics.gcd(dx,dy)
-        print(dx, dy, numerics.gcd(dx,dy))
+        q = math.atan2(dy,dx)
         if acc.index[q] == nil then
           if withinBounds(x, y) and callbackIfBlocked(p[1], p[2]) then
+            acc.index[q]=true
+            acc[#acc+1]={x,y}
           end
         end
       end
       return spiralFromBox(nwcx-1, nwcy-1, secx+1, secy+1, center, acc, callbackIfBlocked)
     end
 
-    local v = spiralFromBox(3, 3, 3, 3, {3,3}, {index={}}, function (x, y)
-                              local res = map[y][x] == '#'
-                              if res then
-                                map[y][x] = 'S'
-                              end
-                              return res
-    end)
+    local max, bx, by = 0, 0, 0
+    for y=1,#map do
+      for x=1,#map[1] do
+        local v = spiralFromBox(x, y, x, y, {x,y}, {index={}}, function (x, y)
+                                  local res = map[y][x] == '#'
+                                  -- if res then
+                                  --   map[y][x] = 'S'
+                                  -- end
+                                  return res
+                               end)
+        if #v > max then
+          max = #v
+          bx, by = x, y
+        end
+      end
+    end
+    print(max, bx, by)
 
     for _,row in ipairs(map) do
       print(table.concat(row, ''))
